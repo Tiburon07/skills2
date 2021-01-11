28bd5548397f9493e4280681c3a77c1c4a523436
t.iordache <t.iordache@geosystems.it>
2021-01-11 15:47:21 +0100

    mappa

--- src/app/map/map-manager/map-manager.component.ts
+++ src/app/map/map-manager/map-manager.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import * as $ from 'jquery';
+import { NgxSpinnerService } from "ngx-spinner";
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-map-manager',
@@ -9,31 +11,38 @@ import * as $ from 'jquery';
 })
 export class MapManagerComponent implements OnInit {
 
-   map!: L.Map;
+  map!: L.Map;
 
-  constructor() {}
+  private tileLayer = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGlidXJvbjA3IiwiYSI6ImNramZ2em85NzNwZDQycG52M3NqbTZsbzQifQ.PyUsvBL-12oKzBldB2CPuA';
+
+  private mapOptions = {
+    //L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+    attribution: '',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+  }
+
+  constructor(private spinner: NgxSpinnerService, private toaster: ToastrService) {}
 
 
   ngOnInit(): void {
 
     this.map = L.map('mapid').setView([41.902782, 12.496366], 11);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGlidXJvbjA3IiwiYSI6ImNramZ2em85NzNwZDQycG52M3NqbTZsbzQifQ.PyUsvBL-12oKzBldB2CPuA', {
-      attribution: '',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'your.mapbox.access.token'
-    }).addTo(this.map);
+    L.tileLayer(this.tileLayer, this.mapOptions).addTo(this.map);
 
-    this.locationHome.bind(this)();
+    //this.locationHome.bind(this)();
 
+   //EVENT MAP
    this.map.locate({ setView: true, maxZoom: 16 });
    this.map.on('locationfound', this.onLocationFound.bind(this));
    this.map.on('locationerror', this.onLocationError.bind(this));
+   this.map.on('click', this.onMapClick.bind(this));
   }
 
-  locationHome(): void {
+/*  locationHome(): void {
     let radius = 5000;
     let icon = {
       icon: L.icon({
@@ -44,9 +53,9 @@ export class MapManagerComponent implements OnInit {
         shadowUrl: '/assets/img/markers/marker-shadow.png'
       })
     };
-/*    L.marker([41.99232, 12.71876], icon).addTo(this.map);
-    L.circle([41.99232, 12.71876], radius).addTo(this.map);*/
-  }
+*//*    L.marker([41.99232, 12.71876], icon).addTo(this.map);
+    L.circle([41.99232, 12.71876], radius).addTo(this.map);*//*
+  }*/
 
   onLocationFound(e: { accuracy: any; latlng: L.LatLngLiteral | L.LatLngTuple; }): void {
     let radius = e.accuracy;
@@ -59,12 +68,20 @@ export class MapManagerComponent implements OnInit {
         shadowUrl: '/assets/img/markers/marker-shadow.png'
       })
     };
-    L.marker(e.latlng, icon).addTo(this.map);
+    L.marker(e.latlng, icon).addTo(this.map).bindPopup('Sono qui!')//.openPopup(e.latlng);
     L.circle(e.latlng, radius).addTo(this.map);
   }
 
   onLocationError(e: { message: any; }) {
-    alert(e.message);
+    this.toaster.error(e.message);
   }
 
+  onMapClick(e: any) {
+    console.log(e);
+    //this.map.setView([e.latlng.lat, e.latlng.lng], 12)
+    L.popup()
+      .setLatLng(e.latlng)
+      .setContent(e.latlng.toString())
+      .openOn(this.map);
+  }
 }
