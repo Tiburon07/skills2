3c146f1b20f500c74cc9f71b74afe88bd2e4f13c
t.iordache <t.iordache@geosystems.it>
2021-01-11 12:29:39 +0100

    rinmosso htacess

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //Install express server
 const express = require('express');
-
 const path = require('path');
 
 const app = express();
@@ -9,8 +8,9 @@ const app = express();
 app.use(express.static('./dist/skills2'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(_dirname, 'dist/skills2/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/skills2/index.html'));
 });
+
 
 //Start the app by listening in the default Heroku prot
 app.listen(process.env.PORT || 8080);
